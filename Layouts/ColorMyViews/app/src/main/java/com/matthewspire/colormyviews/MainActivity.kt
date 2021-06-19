123da075975b0046cd6a6abe262f3640ed5bbd0e
Matthew Spire <matthew_spire@yahoo.com>
2021-06-19 03:18:51 -0700

    Added Three Buttons with Constraints that, When Clicked, Change the Color of Box 3 (Red), Box 4 (Yellow), and Box 5 (Green)

--- Layouts/ColorMyViews/app/src/main/java/com/matthewspire/colormyviews/MainActivity.kt
+++ Layouts/ColorMyViews/app/src/main/java/com/matthewspire/colormyviews/MainActivity.kt
@@ -20,8 +20,11 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         // Use a listOf Views because we set the same click listener on each View
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, constraint_layout)
+            listOf(
+                box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout,
+                red_button, yellow_button, green_button
+            )
 
         // Set the listener for each View
         for (item in clickableViews) {
@@ -55,6 +58,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             // User taps the background, which reveals the outline of other boxes to click as a hint
             else -> view.setBackgroundColor(Color.LTGRAY)
